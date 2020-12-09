f3f7bd2f9e8cb6b53272c3379e9adbef8d3dff53
Carlos Meneses <carlosahz21@gmail.com>
2020-12-09 12:31:09 -0500

    Version inicial

---
+++ src/main/java/com/clearminds/cmh/dtos/Estudiante.java
@@ -0,0 +1,26 @@
+package com.clearminds.cmh.dtos;
+
+public class Estudiante {
+	private int id;
+	private String nombre;
+	private String apellido;
+
+	public Estudiante(int id, String nombre, String apellido) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public Estudiante(String nombre, String apellido) {
+		super();
+		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	@Override
+	public String toString() {
+		return "Estudiante [id=" + id + ", nombre=" + nombre + ", apellido=" + apellido + "]";
+	}
+
+}
