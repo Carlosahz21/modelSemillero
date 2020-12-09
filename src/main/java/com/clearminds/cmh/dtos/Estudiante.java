d195412b8a977a28b2a69e52aca4bdb49e03c975
Carlos Meneses <carlosahz21@gmail.com>
2020-12-09 18:54:50 -0500

    Añadir construtor por defecto de Estudiante

--- src/main/java/com/clearminds/cmh/dtos/Estudiante.java
+++ src/main/java/com/clearminds/cmh/dtos/Estudiante.java
@@ -4,6 +4,10 @@ public class Estudiante {
 	private int id;
 	private String nombre;
 	private String apellido;
+	
+
+	public Estudiante() {
+	}
 
 	public Estudiante(int id, String nombre, String apellido) {
 		super();
@@ -15,6 +19,30 @@ public class Estudiante {
 	public Estudiante(String nombre, String apellido) {
 		super();
 		this.nombre = nombre;
+		this.apellido = apellido;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
 
